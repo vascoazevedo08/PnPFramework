8cbe14e62742a4f69dfbce30eaf76675d070e213
Vasco Azevedo <vasco.azevedo@bindtuning.com>
2025-09-01 15:53:56 +0100

    Add null check before assigning footer display name from template

--- src/lib/PnP.Framework/Provisioning/ObjectHandlers/ObjectSiteFooterSettings.cs
+++ src/lib/PnP.Framework/Provisioning/ObjectHandlers/ObjectSiteFooterSettings.cs
@@ -378,12 +378,12 @@ namespace PnP.Framework.Provisioning.ObjectHandlers
                         // Avoid setting a null DisplayName, which causes errors if the footer was previously enabled.
                         // This can happen when a user disables the footer after it was set.
                         // Only update if DisplayName is present in the template.
-                        if (template.Footer.DisplayName != null)
+                        if (template.Footer?.DisplayName != null)
                         {
                             chrome.Footer.DisplayName = template.Footer.DisplayName;
                         }
                         chrome.Footer.Layout = (PnP.Core.Model.SharePoint.FooterLayoutType)Enum.Parse(typeof(PnP.Core.Model.SharePoint.FooterLayoutType), template.Footer.Layout.ToString());
-                        chrome.Footer.Emphasis = (PnP.Core.Model.SharePoint.FooterVariantThemeType)Enum.Parse(typeof(PnP.Core.Model.SharePoint.FooterVariantThemeType), template.Footer.BackgroundEmphasis.ToString()); 
+                        chrome.Footer.Emphasis = (PnP.Core.Model.SharePoint.FooterVariantThemeType)Enum.Parse(typeof(PnP.Core.Model.SharePoint.FooterVariantThemeType), template.Footer.BackgroundEmphasis.ToString());
 
                         pnpCoreContext.Web.GetBrandingManager().SetChromeOptions(chrome);
                     }
